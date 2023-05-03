9f45958d5190138e17c1940a95bbf09e0e4a52a0
77ORO <77ORO@users.noreply.github.com>
2023-05-03 17:01:28 +0200

    File Runner Service
    Service that runs any command inside a file

---
+++ FileRunnerService/FileRunnerService/Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace FileRunnerService
+{
+    public partial class Service1 : ServiceBase
+    {
+        System.Timers.Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Elapsed += new ElapsedEventHandler(doWork);
+            timer.Interval = 1000 * 60 * 5; // All 5 minutes
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void doWork(object source, ElapsedEventArgs e)
+        {
+            string fileName = "C:\\ProgramData\\input-file.txt";
+            string inputstring = File.ReadAllText(fileName);
+            if (inputstring.Length > 2)
+            {
+                foreach (string line in inputstring.Split('\n'))
+                {
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/C " + line;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                File.WriteAllText(fileName, string.Empty);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,11 @@ Runs PowerShell using System.Management.Automation DLL
 - bypasses constrained language mode
 - bypasses execution policies
 - bypasses PowerShell app blocking
+
+### FileRunner Service
+
+Creates a service that runs any commands located inside the following file: 
+
+	C:\ProgramData\input-file.txt
+
+After execution the file will be emptied. New commands are run after a 5-minute interval.
